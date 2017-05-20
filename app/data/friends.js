e62c456afa53b2b51fb921aa2c345e9ba64dbbaf
Tripp Brogden <mynameistripp@gmail.com>
2017-05-19 21:32:52 -0400

    uploading new files

---
+++ app/data/friends.js
@@ -0,0 +1,114 @@
+var data = [{
+    name: "Chuck the Duck",
+    photo: "http://www.axtell.com/chuckduckcu.jpg",
+    scores: [3,
+        4,
+        5,
+        3,
+        2,
+        1,
+        3,
+        2,
+        1,
+        4
+    ]
+}, {
+    name: "Hugh Mongus",
+    photo: "https://i.ytimg.com/vi/bQPq1VLXY7U/hqdefault.jpg",
+    scores: [5,
+        2,
+        3,
+        4,
+        1,
+        2,
+        5,
+        3,
+        2,
+        4
+    ]
+}, {
+    name: "Derpa the Sherpa",
+    photo: "https://cdn.meme.am/cache/images/folder579/250x250/15283579.jpg",
+    scores: [3,
+        2,
+        4,
+        5,
+        2,
+        1,
+        2,
+        3,
+        3,
+        5
+    ]
+}, {
+    name: "Kira",
+    photo: "https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e15/11015552_401003996739894_1871155561_n.jpg",
+    scores: [3,
+        3,
+        4,
+        2,
+        1,
+        1,
+        3,
+        5,
+        2,
+        3
+    ]
+}, {
+    name: "Bill Nye (not the science guy)",
+    photo: "https://i.ytimg.com/vi/dnga2qjnNrs/hqdefault.jpg",
+    scores: [3,
+        1,
+        4,
+        4,
+        5,
+        3,
+        1,
+        2,
+        3,
+        4
+    ]
+}, {
+    name: "Fitzgerald the dog",
+    photo: "http://www.seniorark.com/Humor/Ugly%20Dogs/ugly%20dog%202.jpg",
+    scores: [2,
+        1,
+        1,
+        3,
+        2,
+        4,
+        3,
+        2,
+        3,
+        1
+    ]
+}, {
+    name: "Cookie",
+    photo: "http://www.jokesoftheday.net/jokes-archive/2016/02/09/Grandma-21.jpg",
+    scores: [5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+}, {
+    name: "Bowl of Salt",
+    photo: "http://www.theguardian.pe.ca/content/dam/tc/the-guardian/images/2017/1/3/tg-03012017-salt.jpg",
+    scores: [1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+}];
+module.exports = data;
